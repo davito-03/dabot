8dbd730d6353b78907d56beadd08f2432591dd4b
David Doleo <soy@davito.es>
2025-02-18 20:16:08 +0100

    update

--- main.py
+++ main.py
@@ -1,43 +1,10 @@
 import discord
 from discord.ext import commands, tasks
 import random
-import asyncio
-import praw
-import json
-from flask import Flask
-from pyngrok import ngrok
-from google.cloud import translate_v2 as translate
-from threading import Thread
 import requests
 from googletrans import Translator
 from dotenv import load_dotenv
 import os
-import time
-import sympy as sp
-
-
-app = Flask(__name__)
-public_url = ngrok.connect(8080)
-print(f"Túnel público generado: {public_url}")
-
-@app.route("/")
-def home():
-    return "¡Hola Mundo!"
-
-def run():
-    app.run(host='0.0.0.0', port=5000)
-
-Thread(target=run).start()
-
-def traducir_palabra(palabra):
-    url = 'https://libretranslate.de/translate'
-    params = {
-        'q': palabra,
-        'source': 'en',
-        'target': 'es'
-    }
-    response = requests.post(url, data=params)
-    return response.json()['translatedText']
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -48,15 +15,9 @@ intents.members = True
 intents.presences = True
 bot = commands.Bot(command_prefix='dabot ', intents=intents)
 
-last_message_time = {}
+# Se limpia la lista de chistes y citas usados cada 30 minutos
 used_jokes = []  
 used_quotes = []  
-
-reddit = praw.Reddit(
-    client_id=os.getenv('REDDIT_CLIENT_ID'),
-    client_secret=os.getenv('REDDIT_SECRET'),
-    user_agent=os.getenv('REDDIT_USER_AGENT')
-)
 
 @tasks.loop(minutes=30)
 async def clear_used_data():
@@ -77,34 +38,34 @@ async def ping(ctx):
 @bot.command()
 async def adivina_palabra(ctx):
     await ctx.send("¿Cuál es la dificultad que prefieres? (fácil, medio, difícil)")
+
     def check(msg):
         return msg.author == ctx.author and msg.channel == ctx.channel
 
     msg = await bot.wait_for('message', check=check)
     dificultad = msg.content.lower()
 
-    if dificultad == 'fácil':
-        min_len, max_len = 3, 5
-    elif dificultad == 'medio':
-        min_len, max_len = 6, 8
-    elif dificultad == 'difícil':
-        min_len, max_len = 9, 12
-    else:
-        await ctx.send("Dificultad no válida. Usando 'medio'.")
-        min_len, max_len = 6, 8
+    # Mapeo de dificultades a rangos de longitud
+    dificultad_map = {
+        'fácil': (3, 5),
+        'medio': (6, 8),
+        'difícil': (9, 12)
+    }
+
+    min_len, max_len = dificultad_map.get(dificultad, (6, 8))  # Valor predeterminado: medio
 
     response = requests.get(f"https://random-word-api.herokuapp.com/word?number=1")
-    word = response.json()[0]  
+    word = response.json()[0]
 
     while not (min_len <= len(word) <= max_len):
         response = requests.get(f"https://random-word-api.herokuapp.com/word?number=1")
         word = response.json()[0]
 
-    palabra_traducida = traducir_palabra(word)
+    palabra_traducida = word
     guessed_word = ['_'] * len(palabra_traducida)
-    attempts = 6  
+    attempts = 6
 
-    await ctx.send(f"¡Adivina la palabra! Tienes {attempts} intentos. La palabra traducida tiene {len(palabra_traducida)} letras.")
+    await ctx.send(f"¡Adivina la palabra! Tienes {attempts} intentos. La palabra tiene {len(palabra_traducida)} letras.")
 
     while attempts > 0 and '_' in guessed_word:
         await ctx.send(f"Palabra: {''.join(guessed_word)}")
@@ -127,7 +88,6 @@ async def adivina_palabra(ctx):
     if attempts == 0:
         await ctx.send(f"Se te acabaron los intentos. La palabra era: {palabra_traducida}")
 
-
 @bot.command()
 async def chiste_todos(ctx):
     try:
@@ -142,18 +102,12 @@ async def chiste_todos(ctx):
             await ctx.send(f"Error en los datos recibidos de la API: {joke_data['error']}")
             return
 
-        if joke_data['type'] == 'single':
-            joke = joke_data['joke']
-        elif joke_data['type'] == 'twopart':
-            joke = f"{joke_data['setup']} - {joke_data['delivery']}"
+        joke = joke_data['joke'] if joke_data['type'] == 'single' else f"{joke_data['setup']} - {joke_data['delivery']}"
 
         while joke in used_jokes:
             response = requests.get("https://v2.jokeapi.dev/joke/Miscellaneous?lang=en")
             joke_data = response.json()
-            if joke_data['type'] == 'single':
-                joke = joke_data['joke']
-            elif joke_data['type'] == 'twopart':
-                joke = f"{joke_data['setup']} - {joke_data['delivery']}"
+            joke = joke_data['joke'] if joke_data['type'] == 'single' else f"{joke_data['setup']} - {joke_data['delivery']}"
 
         used_jokes.append(joke)
 
@@ -180,18 +134,12 @@ async def chiste(ctx):
             await ctx.send(f"Error en los datos recibidos de la API: {joke_data['error']}")
             return
 
-        if joke_data['type'] == 'single':
-            joke = joke_data['joke']
-        elif joke_data['type'] == 'twopart':
-            joke = f"{joke_data['setup']} - {joke_data['delivery']}"
+        joke = joke_data['joke'] if joke_data['type'] == 'single' else f"{joke_data['setup']} - {joke_data['delivery']}"
 
         while joke in used_jokes:
             response = requests.get("https://v2.jokeapi.dev/joke/Any?safe-mode")
             joke_data = response.json()
-            if joke_data['type'] == 'single':
-                joke = joke_data['joke']
-            elif joke_data['type'] == 'twopart':
-                joke = f"{joke_data['setup']} - {joke_data['delivery']}"
+            joke = joke_data['joke'] if joke_data['type'] == 'single' else f"{joke_data['setup']} - {joke_data['delivery']}"
 
         used_jokes.append(joke)
 
@@ -252,52 +200,5 @@ async def guessnum(ctx):
                 break
         except ValueError:
             await ctx.send("Por favor, ingresa un número válido.")
-
-@bot.command()
-async def operaciones(ctx, operacion: str):
-    try:
-        result = eval(operacion)
-        await ctx.send(f"El resultado de {operacion} es: {result}")
-    except Exception as e:
-        await ctx.send(f"No se pudo realizar la operación. Detalles: {e}")
-
-@bot.command()
-async def cita(ctx):
-    try:
-        response = requests.get("https://zenquotes.io/api/random")
-        quote_data = response.json()
-
-        quote = quote_data[0]['q']
-        author = quote_data[0]['a']
-
-        # Evitar que se repita la cita
-        while quote in used_quotes:
-            response = requests.get("https://zenquotes.io/api/random")
-            quote_data = response.json()
-            quote = quote_data[0]['q']
-            author = quote_data[0]['a']
-
-        used_quotes.append(quote)
-
-        quote_text = f"**{quote}**\nPor: {author}"
-
-        translator = Translator()
-        translated_quote = translator.translate(quote_text, src='en', dest='es').text
-
-        await ctx.send(f"Aquí tienes una cita:\n\n{translated_quote}")
-
-    except Exception as e:
-        await ctx.send(f"Hubo un error al obtener la cita. Detalles: {e}")
-        print(f"Error: {e}")
-
-@bot.command()
-async def resolver(ctx, *args):
-    try:
-        exp = " ".join(args)
-        expr = sp.sympify(exp)
-        result = sp.solve(expr)
-        await ctx.send(f"Solución: {result}")
-    except Exception as e:
-        await ctx.send(f"No se pudo resolver la ecuación. Detalles: {e}")
-
+            
 bot.run(TOKEN)
